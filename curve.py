9667691b5ef39f6ed35921249b873a9e60f4270a
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2016-03-29 17:16:34 -0400

    Finilized Structure

---
+++ curve.py
@@ -0,0 +1,18 @@
+
+
+class Curve(object):
+	"""
+
+	"""
+
+	def __init__(self, points):
+		self.points = points
+
+	def derivative(self):
+		pass
+
+	def integral(self):
+		pass
+
+	def smoothen(self):
+		pass
