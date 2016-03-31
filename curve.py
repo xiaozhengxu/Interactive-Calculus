bcefe9512e9df5e8b3dba0ab69b516457061919f
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2016-03-30 20:25:49 -0400

    Added Class fundamentals: attributes are points, smooth, derivative and integral. The first is the raw set of points given for construction, smooth is the interpolated (smoothened) version. Smoothen function implemented. move_point function created, but needs tweaking.

--- curve.py
+++ curve.py
@@ -1,4 +1,7 @@
-
+# from scipy.interpolate import interp1d
+#import scipy
+from scipy import interpolate
+import numpy as np
 
 class Curve(object):
 	"""
@@ -7,12 +10,110 @@ class Curve(object):
 
 	def __init__(self, points):
 		self.points = points
+		self.update()
 
-	def derivative(self):
+	def deep_copy(self):
+		return Curve(self.points[:])
+
+	def update(self):
+		self.smooth = self.smoothen()
+		self.derivative = self.derive()
+		self.integral = self.integrate()
+
+
+	def derive(self):
 		pass
 
-	def integral(self):
+	def integrate(self):
 		pass
 
 	def smoothen(self):
-		pass
+		# x = [tup[0] for tup in self.points]
+		# y = [tup[1] for tup in self.points]
+		#sorted_pts = sorted(self.points, key=lambda point: point[0])
+		# sorted_pts = sorted(self.points)
+
+		#unzip = zip(*sorted_pts)
+		unzip = zip(*self.points)
+		x = unzip[0]
+		y = unzip[1]
+
+		# print 'x=', x, type(x)
+		# print 'y=', y, type(y)
+
+		interpol = interpolate.interp1d(x,y, kind='cubic')
+
+		xnew = np.linspace(x[0]+1, x[-1]-1, (x[-1] - x[0]))
+		ynew = interpol(xnew)
+		# ynew = np.interp(xnew, x, y)
+
+		return zip(xnew, ynew)
+
+	def move_point(self, index, distance, kind='absolute'): # currently doesn't change the local curve
+		pts = self.points
+		distance = float(distance)
+
+		if index >= len(pts):
+			print 'Index out of Range'
+			return None
+
+		# pts[index] = (pts[index][0], pts[index][1] + distance)
+
+		if kind == 'absolute':
+			# Absolute change in distance
+			for i, pt in enumerate(pts[:index]):
+				pts[i] = (pts[i][0], pts[i][1] + distance / (index-i+1))
+				print 'moved', distance / (index-i+1)
+
+			for i, pt, in enumerate(pts[index:]):
+				pts[i+index] = (pts[i+index][0], pts[i+index][1] + distance / (i+1))
+				print 'moved', distance / (i+1)
+		elif kind == 'relative':
+			rel_d = distance / pts[index][1] 
+			for i, pt in enumerate(pts[:index]):
+				pts[i] = (pts[i][0], pts[i][1] * (1 + (rel_d / (index-i+1)))**2)
+				print 'moved', 1 + rel_d / (index-i+1)
+
+			for i, pt, in enumerate(pts[index:]):
+				pts[i+index] = (pts[i+index][0], pts[i+index][1] * (1 + (rel_d / (i+1)))**2)
+				print 'moved', 1 + rel_d / (i+1)
+		else:
+			print 'Invalid Version'
+			return None
+
+
+		self.update()
+		return pts
+
+
+
+
+if __name__ == '__main__':
+	import matplotlib.pyplot as plt
+
+	# Hardcoded points
+	pts = [(20, 438), (43, 395), (90, 317), (160, 263), (213, 261), (291, 285), (379, 282), (468, 228), (544, 142), (598, 55), (609, 39)]
+	# pts = [(79, 330), (81, 317), (88, 266), (108, 221), (170, 165), (246, 127), (333, 133), (375, 196), (366, 260), (316, 281), (275, 258), (260, 218), (304, 145), (365, 113), (478, 129), (556, 172), (584, 183)]
+	# pts = [(1,1), (2,4), (3,9), (4,16), (5,25)]
+
+
+	pts = [(29, 204), (46, 204), (72, 201), (104, 200), (136, 209), (179, 233), (228, 278), (279, 304), (339, 302), (381, 274), (424, 240), (460, 227), (511, 227), (535, 224), (557, 224), (576, 224), (577, 224)]
+
+	xy = zip(*pts)
+	print xy
+
+	# Make a curve object
+	c = Curve(pts)
+	c2 = c.deep_copy()
+	c2.move_point(8,-100, kind='absolute')
+
+	m_xy = zip(*c2.points)
+
+	xynew = zip(*c.smooth)
+	xymoved = zip(*c2.smooth)
+
+
+	plt.plot(xy[0], xy[1], 'o' , xynew[0], xynew[1], '-',m_xy[0], m_xy[1], 'o',  xymoved[0], xymoved[1], '+')
+	# plt.plot(xy[0], xy[1], 'o' ,m_xy[0], m_xy[1], '+')
+	plt.show()
+
