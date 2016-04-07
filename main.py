91f92703fba998848bd68af3c54965a6afbc8c08
mesrumpled <marchsaper@gmail.com>
2016-04-06 21:12:54 -0400

    I think this is how this should go?

--- main.py
+++ main.py
@@ -7,34 +7,15 @@ from curve import Curve
 
 if __name__ == "__main__":
 
-	model = Model()
+	model = Curve()
 
-	view = View()
+	view = View(curve)
 
-	# mouse_control = Mouse_control()
+	control = Control(model)
 
-	# open_cv = Open_CV_control()
-
-	control = Control(mouse_control, open_cv)
-
-	# screen.fill( (255,255,255) )
-
-	# running = True
-	# while running:
-	# 	pygame.time.wait(100)
-	# 	# draw
+	
 
     while control.running:
     	control.handle_event()
 
-    	view.update()
-
-		# for event in pygame.event.get():
-		# 	if event.type == pygame.QUIT:
-		# 		running = False
-
-		# 	mouse_control.handle_event(event)
-
-		# 	mouse_control.print_points()
-
-			# Open CV event
+        view.update()
