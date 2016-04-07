542651f2b9a955c45c394719074870251f9a2618
mesrumpled <marchsaper@gmail.com>
2016-04-06 21:08:54 -0400

    pushing cleaned

--- view.py
+++ view.py
@@ -25,12 +25,13 @@ class View(object):
 
 
     def draw(self):
-        fig = pylab.figure(figsize=[4, 4], # Screen Size in inches
+        fig = pylab.figure(figsize=[6, 6], # Screen Size in inches
                    dpi=100,        # 100 dots per inch, so the resulting buffer is XxY pixels
                    )
         ax = fig.gca()  # The matplotlib figure will be non-interactive
         #ax.plot(self.curve.ATTRIBUTE)   #!!!
-        ax.plot([1, 2, 3, 3])   #!!!!
+        ax.plot([1, 2, 3, 3], numpy.linspace(1, len([1, 2, 3, 3]), num = len([1, 2, 3, 3])), [1, 4, 3, 2], [1, 2, 3, 4])  #!!!!
+
          
         canvas = agg.FigureCanvasAgg(fig)
         canvas.draw()   # Non-interactive figures must be manually updated
@@ -38,7 +39,7 @@ class View(object):
         raw_data = renderer.tostring_rgb()  # The raw data (matrix) from the matplotlib graph can now be u1sed by pygame
          
 
-        window = pygame.display.set_mode((600, 400), DOUBLEBUF) # Includes window size 
+        window = pygame.display.set_mode((600, 600), DOUBLEBUF) # Includes window size 
         self.screen = pygame.display.get_surface()
          
         size = canvas.get_width_height()
