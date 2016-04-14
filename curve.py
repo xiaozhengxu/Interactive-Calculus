ac584999160c4a8c937dfb9d9ce0f99977746039
mesrumpled <marchsaper@gmail.com>
2016-04-13 21:51:25 -0400

    Documented

--- curve.py
+++ curve.py
@@ -1,20 +1,28 @@
-# from scipy.interpolate import interp1d
-#import scipy
 from scipy import interpolate
 import numpy as np
 
 class Curve(object):
 	"""
+	Takes in an of tuples. Curve creates three Line objects which are the input line, derivative of that line and integral of the same. These are initialized in the init. 
+	Curve has a method move_point which allows the user to click and drag graphed lines, seeing how the line, derivative and integral change. 
 
 	"""
 
 	def __init__(self, points):
+		"""
+		Initializes the input points as a Line and finds the corresponding derivative and integral through methods of Line. Input points must be smoothed.
+		The derivative and integral of a smooth line will be smooth without smoothening.
+		"""
 		self.line = Line(points)
 		self.derivative = Line(self.line.derive(), smooth_bool=True)
-		# print self.derivative.points
 		self.integral = Line(self.line.integrate(), smooth_bool=True)
 
 	def move_point(self, handle, distance, line='line'):
+		"""
+		This method adjusts the three related lines as the user clicks and drags points on one of them. If the input line is changed, the derivative and integral
+		are changed. If the derivative is changed, the line and integral are changed. In this case the new line is found through integrating the changed derivative.
+		A similar process happens if the integral is changed.
+		"""
 		if line == 'line':
 			self.line.move_point(handle, distance)
 
@@ -24,16 +32,19 @@ class Curve(object):
 		elif line == 'derivative':
 			self.derivative.move_point(handle, distance)
 
-			self.line = Line(self.derivative.integrate(), smooth_bool=True)
-			self.integral = Line(self.line.integrate(), smooth_bool=True)
+			self.line = Line(self.derivative.integrate(), smooth_bool=True)	# The new line will be the integral of the changed derivative
+			self.integral = Line(self.line.integrate(), smooth_bool=True)	# Back to normal
 
 		elif line == 'integral':
 			self.integral.move_point(handle, distance)
 
-			self.line = Line(self.integral.derive(), smooth_bool=True)
-			self.derivative = Line(self.line.derive(), smooth_bool=True)
+			self.line = Line(self.integral.derive(), smooth_bool=True) # The new line will be the derivative of the changed integral
+			self.derivative = Line(self.line.derive(), smooth_bool=True)	# Back to normal
 
 	def draw_to_plot(self, variables=['line', 'derivative', 'integral']):
+		"""
+		This method is for testing purposes. It plots the line, integral and derivative in matplotlib.
+		"""
 
 		if any(var == 'line' for var in variables):
 			# print 'Plotting line'
@@ -60,25 +71,38 @@ class Curve(object):
 
 class Line(object):
 	"""
-
+	A class that takes in list of points. Contains attributes to smooth, derive, integrate, and adjust itself.
 	"""
 
-	def __init__(self, points, pull_pts_num=7, smooth_bool=False):		
+	def __init__(self, points, pull_pts_num=7, smooth_bool=False):	
+	"""
+    Initializes a line class. If the line is not considered smooth, the smoothen() method is called.
+	"""	
 		print "Making Line"
 		if smooth_bool:		# If the points are smooth, don't bother smoothening the curve
 			self.points = points
 			print len(points)
-			self.pull_points = (0,0)#points[::len(points)/pull_pts_num]
+			self.pull_points = (0,0)  
+			#points[::len(points)/pull_pts_num]
 		else:
 			self.points, self.pull_points = self.smoothen(points, pull_pts_num=pull_pts_num)
 
 	def __index__(self, idx):
+		"""
+        Method that allows index of Line to be called. Ex: Line[index] will work as if Line was a list. 
+		"""
 		return self.points[idx]
 
 	def deep_copy(self):
+		"""
+        Method that allows a Line object to be copied. 
+		"""
 		return Line(self.points[:])
 
 	def smoothen(self, points, pull_pts_num=3):
+		"""
+        Smooths input points. Since user drawn points may not be smooth, this method smooths them using scipy's interpolation.
+		"""
 		unzip = zip(*points)	# unzip the (x,y) tuples into a list of x and a list of y
 		x = unzip[0]
 		y = unzip[1]
@@ -97,14 +121,15 @@ class Line(object):
 		return (zip(xnew, ynew), zip(xpull, ypull))			# zip the list of x and list of y into (x,y) tuples
 
 	def move_point(self, index, distance, kind='relative'): # currently doesn't change the local Line
+	"""
+    Method to adjust the points of a line as it is adjusted/dragged through user input. A line is adjusted when the user pulls its "pulling points". 
+	"""
 		pts = self.points
-		distance = float(distance)
+		distance = float(distance)	# Distance pulling point moved
 
 		if index >= len(pts):
 			print 'Index out of Range'
 			return None
-
-		# pts[index] = (pts[index][0], pts[index][1] + distance)
 
 		if kind == 'absolute':
 			factor = 1.5
@@ -117,23 +142,14 @@ class Line(object):
 				pts[i+index] = (pts[i+index][0], pts[i+index][1] + distance / (i+1)**2)
 				print 'moved', distance / (i+1)
 
-			# # Absolute change in distance
-			# for i, pt in enumerate(pts[:index]):
-			# 	pts[i] = (pts[i][0], pts[i][1] + distance * i / index)
-			# 	print 'moved', distance / (index-i+1)
-
-			# for i, pt, in enumerate(pts[index:]):
-			# 	pts[i+index] = (pts[i+index][0], pts[i+index][1] + distance * (index-i) / index)
-			# 	print 'moved', distance / (i+1)
-
 		elif kind == 'relative':
 			rel_d = distance / pts[index][1] 
 			for i, pt in enumerate(pts[:index]):
-				pts[i] = (pts[i][0], pts[i][1] * (1 + (rel_d / (index-i+1))))
+				pts[i] = (pts[i][0], pts[i][1] * (1 + (rel_d / (index-i+1))))	# For the line to the left of the adjusted point, shift each index accordintly. The adjustment becomes larger, closer to the point that was adjusted.
 				print 'moved', 1 + rel_d / (index-i+1)
 
 			for i, pt, in enumerate(pts[index:]):
-				pts[i+index] = (pts[i+index][0], pts[i+index][1] * (1 + (rel_d / (i+1))))
+				pts[i+index] = (pts[i+index][0], pts[i+index][1] * (1 + (rel_d / (i+1))))	# Similar to above
 				print 'moved', 1 + rel_d / (i+1)
 		else:
 			print 'Invalid Version'
@@ -145,37 +161,29 @@ class Line(object):
 
 	
 	def derive(self):
-		# print list(np.gradient(self.points))
-		# print 'in derive()'
-
+		"""
+        Method that creates a list of tuples which is the derivative of the current Line object.
+		"""
 		deriv = []
 		prev_pt = self.points[0]
 
 		for pt in self.points[1:]:
-			deriv.append( ((pt[0]+prev_pt[0])/2.0, 50*(prev_pt[1]-pt[1])/(prev_pt[0]-pt[0])+500) ) # Note: Weird scaling
+			deriv.append( ((pt[0]+prev_pt[0])/2.0, 50*(prev_pt[1]-pt[1])/(prev_pt[0]-pt[0])+500) ) # Numerical approximation. Note: Weird scaling
 			prev_pt = pt
 
 		return deriv
 
-		# grad = np.gradient(self.points)
-		# ret = []
-		# for pt in grad:
-		# 	print pt[0], pt[1]
-		# 	ret.append((pt[0], pt[1]))
-		# return ret
-
 	def integrate(self):
+		"""
+		Method that creates a list of tuples which is the integral of the current Line object.
+		"""
 		integral = []
 		prev_pt = self.points[0]
-
-		tr = 0.0
-
-		C = 0
-
+		tr = 0.0	# Initial trapezoidal area
+		C = 0	# Initial integration constant
 		for pt in self.points[1:]:
-			# integral.append(((pt[0]+prev_pt[0])/2, np.trapz([prev_pt[1], pt[1]], [prev_pt[0], pt[0]])))
-			tr += trap((prev_pt[0]-500, prev_pt[1]-500), (pt[0]-500, pt[1]-500))
-			x = (pt[0]+prev_pt[0])/2
+			tr += trap((prev_pt[0]-500, prev_pt[1]-500), (pt[0]-500, pt[1]-500))	# Numerical approximation of integral
+			x = (pt[0]+prev_pt[0])/2	# New x value
 
 			if x < 500:				# Get the integration constant (integral from the first point to 0)
 				C = tr
@@ -185,9 +193,7 @@ class Line(object):
 			integral.append(int_pt)
 			prev_pt = pt
 
-
-		integral = [(pt[0], 500+(pt[1]-C)/500) for pt in integral]
-
+		integral = [(pt[0], 500+(pt[1]-C)/500) for pt in integral] # WEIRD SCALING 
 		return integral
 
 def trap(a, b):
