427a09c0b83e61d6867a7645e0c5897d892e06a2
xiaozhengxu <xiaozheng.xu@students.olin.edu>
2016-04-13 21:19:29 -0400

    Update Control.py

--- Control.py
+++ Control.py
@@ -111,15 +111,6 @@ class Mouse_control(object):
 		self.last_press = False
 
 	def handle_event(self):
-		'''This method is currently called by view.draw_input()
-
-		Allows the user to draw several lines/curves in discrete intervals with mouse. 
-		Press leftbutton to start drawing, move around the mouse to draw (or hold down the lef button while drawing.
-		Press leftbutton again to stop drawing. Press rightbutton to clear screen.
-
-		running_points stores the points of the user's curve as nested lists. (if the user draws a single curve, it would be [[(x,y)...]]
-
-		Next implementation would be to stop drawing when the leftbutton is released (MOUSEBUTTONUP doesn't work right now).'''
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.running = False
@@ -159,6 +150,16 @@ class Mouse_control(object):
 
 	def print_points(self):
 		print self.running_points
+
+# '''This method is currently called by view.draw_input()
+
+# 		Allows the user to draw several lines/curves in discrete intervals with mouse. 
+# 		Press leftbutton to start drawing, move around the mouse to draw (or hold down the lef button while drawing.
+# 		Press leftbutton again to stop drawing. Press rightbutton to clear screen.
+
+# 		running_points stores the points of the user's curve as nested lists. (if the user draws a single curve, it would be [[(x,y)...]]
+
+# 		Next implementation would be to stop drawing when the leftbutton is released (MOUSEBUTTONUP doesn't work right now).'''
 
 
 # if __name__ == "main":
